84107281d3f3a7c9138309049ce20b404ccb4eef
Alexandr-Bychkov <lalka.lol1@mail.ru>
2021-04-11 11:50:41 +0300

    вынес сортировку в функцию

---
+++ sort_p.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+
+using namespace std;
+int n;
+int sort(int *A) {
+	
+	int temp; // временная переменная для обмена элементов местами
+for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (A[j] > A[j + 1]) {
+				// меняем элементы местами
+				temp = A[j];
+				A[j] = A[j + 1];
+				A[j + 1] = temp;
+			}
+		}
+	}
+return *A;
+}
+
+int main()
+{
+	int *A; // указатель для  массива
+	// Ввод количества элементов массива
+	cout << "n = ";
+	cin >> n;
+
+	A = new int[n]; // создание массива размера n
+	
+	// заполнение массива
+	cout << "vvedite elementi massiva = ";
+	for (int i = 0; i < n; i++) {
+		cin >> A[i];
+	}
+
+	
+
+	// Сортировка массива 
+	sort(A);
+
+	// Вывод отсортированного массива
+	for (int i = 0; i < n; i++) {
+		cout << A[i] << " ";
+	}
+	cout << endl;
+
+	delete[] A; // удаление массива
+
+	return 0;
+}
